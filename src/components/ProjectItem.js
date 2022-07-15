6d48a4c05e9af3904d999b906e409625e548bfad
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-07-15 22:51:10 +0300

    Different rendering method

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,18 +1,20 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
-  const techs = technologies.map((elem, index) => {
-    return(
-      <span key={index}>{elem}</span>
-    )
-  })
+  // const techs = technologies.map((elem, index) => {
+  //   return(
+  //     <span key={index}>{elem}</span>
+  //   )
+  // })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
-        {techs}
+        {/* {techs} */}
+        {technologies.map((elem, index) => {return(<span key={index}>{elem}</span>)
+        })}
       </div>
     </div>
   );
