8c5d585b29a7f024a316fa2734796a3f03028f87
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-07-15 22:34:09 +0300

    Assignment Complete

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,17 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const projectItemElements = projects.map((elem) => {
+    return (
+      <ProjectItem key={elem.id} name={elem.name} about={elem.about} technologies = {elem.technologies} />
+    )
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */}
+      {projectItemElements}
+      </div>
     </div>
   );
 }
